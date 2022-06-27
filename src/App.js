94e1f5642536ba2dd48df1218003d20c0087bb79
GA-Eduardo <107960254+Api-Nasa@users.noreply.github.com>
2022-06-27 02:14:27 +0200

    Landing page cambios
    Landing page cambios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
+import logo from '../src/cool.gif';
+import atom from '../src/atom.gif';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="titulo">
+          <img src={atom} className="App-logo logo-peq" alt="logo" />
+         
+          <h1 className="titulo-landing">React.js Api-Nasa Web proyect. </h1>
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Proyecto <code>Claudia y Eduardo</code> Api-Nasa Web .
-        </p>
-        <button type="button" class="btn btn-primary edu">Btn prueba para claudia </button>
+
+        <button type="button" class="btn btn-primary edu ">
+          Get started for discover{' '}
+        </button>
       </header>
     </div>
   );
